ecceea67a1a08e2e2914cfc90bc56746a5cc3706
Amine Elmouradi <elmouradi.amine.dev@gmail.com>
2024-02-17 21:06:02 +0100

    Add nvim tmux navitor

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -249,4 +249,22 @@ return {
       end,
     },
   },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    event = "VeryLazy",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
+      nvim_tmux_nav.setup({
+        disable_when_zoomed = true,
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        },
+      })
+    end,
+  },
 }
